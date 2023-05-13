f4cc59e76eae8053e70e1bed2a5c8b9d845643f2
Watcharapong Chaidee <ne7shii@gmail.com>
2023-05-14 01:44:32 +0700

    update v2

--- src/components/PlayerCountv2.tsx
+++ src/components/PlayerCountv2.tsx
@@ -35,7 +35,7 @@ const PlayerCount = (props: Props) => {
         setIsTooltipOpen(true);
     }, []); // open tooltip on mount
     return (
-        <div className={`flex min-h-screen flex-col items-center gap-5 px-7 bg-[url('/bg-w-text.png')] bg-contain bg-no-repeat`}>
+        <div className={`flex h-[1080px]  w-[1920px] flex-col items-center gap-5 px-7 bg-[url('/bg-w-text.png')] bg-contain bg-repeat-y `}>
             {/* <div className='text-white text-xl mt-3'>EVENT 1.0</div>
             <div className='flex flex-col items-center'>
                 <div className='text-lg mt-4 text-white md:text-2xl'>คุณอยากเล่นเกม FiveM แล้วได้รับของรางวัลแบบสุดพิเศษไหม? </div>
@@ -50,7 +50,7 @@ const PlayerCount = (props: Props) => {
                 </span>
             </div>
             */}
-            <div className={`text-[10rem] font-bold bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text mt-80 text-transparent ${!props.count && 'invisible'}`}>
+            <div className={`text-[10rem] font-bold text-white mt-96  ${!props.count && 'invisible'}`}>
                 {props.count || '0'}
             </div>
             <div className='relative max-w-6xl w-full'>
@@ -62,7 +62,7 @@ const PlayerCount = (props: Props) => {
                 </Progress.Root>
                 <Tooltip.Provider>
                     {goalValues.map(goal => (
-                        <Tooltip.Root key={goal.labelValue} open={isTooltipOpen}>
+                        <Tooltip.Root key={goal.labelValue} open={isTooltipOpen} >
                             <Tooltip.Trigger asChild>
                                 <div className={`absolute rounded-full bg-black h-8 w-8 -top-2 left-[${goal.position}] `} style={{ transform: `translateX(-${50}%)` }} />
                             </Tooltip.Trigger>
@@ -74,7 +74,7 @@ const PlayerCount = (props: Props) => {
 
                             </Tooltip.Portal>
                             <Tooltip.Portal>
-                                <Tooltip.Content className="bg-white p-2 rounded-xl" sideOffset={10} side='bottom' align='end' alignOffset={-5}>
+                                <Tooltip.Content className="bg-white p-2 rounded-xl" sideOffset={10} side='bottom' forceMount align='end' alignOffset={-5} avoidCollisions={false}>
                                     <div className=' lg:w-40 lg:h-40 w-20 h-20'></div>
                                     <Tooltip.Arrow className="fill-white w-3 h-3" />
                                 </Tooltip.Content>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { type AppType } from "next/app";
-
+import { Mitr } from '@next/font/google';
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
+const mitr = Mitr({ weight:'400'});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={mitr.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 };
 
 export default trpc.withTRPC(MyApp);
