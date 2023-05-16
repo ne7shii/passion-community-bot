622e9b0e4ccfc9a82194d7b812593d9db5cc62f4
Watcharapong Chaidee <ne7shii@gmail.com>
2023-05-16 11:20:15 +0700

    update style

--- src/components/PlayerCountv2.tsx
+++ src/components/PlayerCountv2.tsx
@@ -1,6 +1,7 @@
 import * as Progress from '@radix-ui/react-progress';
 import * as Label from '@radix-ui/react-label';
 import * as Tooltip from '@radix-ui/react-tooltip';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 type Props = {
@@ -8,24 +9,29 @@ type Props = {
 }
 type GoalValue = {
     position: string,
-    labelValue: string
+    labelValue: string,
+    imgUrl: string,
 }
 const goalValues: GoalValue[] = [
     {
         position: '25%',
         labelValue: '300',
+        imgUrl: '/reward2.png'
     },
     {
         position: '50%',
         labelValue: '600',
+        imgUrl: '/reward2.png'
     },
     {
         position: '75%',
         labelValue: '900',
+        imgUrl: '/reward3.png'
     },
     {
         position: '100%',
         labelValue: '1200',
+        imgUrl: '/reward4.png'
     },
 ]
 const PlayerCount = (props: Props) => {
@@ -35,8 +41,11 @@ const PlayerCount = (props: Props) => {
         setIsTooltipOpen(true);
     }, []); // open tooltip on mount
     return (
-        <div className={`flex h-[1080px]  w-[1920px] flex-col items-center gap-5 px-7 bg-[url('/bg-w-text.png')] bg-contain bg-no-repeat ` } >
-            {/* <div className='text-white text-xl mt-3'>EVENT 1.0</div>
+
+        <div className={`w-full h-full bg-[url('/bg-w-text.png')]  `}>
+            <div className='w-full h-full overflow-scroll flex justify-center backdrop-blur-3xl'>
+                <div className={`flex min-h-[1080px]  min-w-[1920px] flex-col items-center gap-5 px-7 bg-[url('/bg-w-text.png')] bg-contain bg-no-repeat `} >
+                    {/* <div className='text-white text-xl mt-3'>EVENT 1.0</div>
             <div className='flex flex-col items-center'>
                 <div className='text-lg mt-4 text-white md:text-2xl'>คุณอยากเล่นเกม FiveM แล้วได้รับของรางวัลแบบสุดพิเศษไหม? </div>
                 <div className='text-lg text-white md:text-2xl'> ถ้าคุณอยากได้ มาเข้าร่วมกิจกรรมของเรากันเถอะ!</div>
@@ -50,44 +59,53 @@ const PlayerCount = (props: Props) => {
                 </span>
             </div>
             */}
-            <div className={`text-[10rem] font-bold text-white mt-96  ${!props.count && 'invisible'}`}>
-                {props.count || '0'}
+                    <div className={`text-[10rem] font-bold text-white mt-96  ${!props.count && 'invisible'}`}>
+                        {props.count || '0'}
+                    </div>
+                    <div className='relative max-w-6xl w-full'>
+                        <Progress.Root className="w-full absolute bg-gray-500 h-4 rounded-full overflow-hidden mb-56" value={props.count}>
+                            <Progress.Indicator
+                                className="bg-red-500 rounded-full w-full h-full"
+                                style={{ transform: `translateX(-${100 - currentPercent}%)` }}
+                            />
+                        </Progress.Root>
+                        <Tooltip.Provider>
+                            {goalValues.map(goal => (
+                                <Tooltip.Root key={goal.labelValue} open={isTooltipOpen} >
+                                    <Tooltip.Trigger asChild>
+                                        <div className={`absolute rounded-full bg-black h-8 w-8 -top-2 left-[${goal.position}] `} style={{ transform: `translateX(-${50}%)` }} />
+                                    </Tooltip.Trigger>
+                                    <Tooltip.Portal>
+                                        <Tooltip.Content className="bg-white p-2 rounded-full" sideOffset={5} >
+                                            {goal.labelValue}
+                                            <Tooltip.Arrow className="fill-white" />
+                                        </Tooltip.Content>
+
+                                    </Tooltip.Portal>
+                                    <Tooltip.Portal>
+                                        <Tooltip.Content className="p-2 rounded-xl" sideOffset={10} side='bottom' forceMount align='center' alignOffset={-5} avoidCollisions={false}>
+                                            {/* <div className=' lg:w-40 lg:h-40 w-20 h-20'></div> */}
+                                            {/* <Tooltip.Arrow className="fill-white w-3 h-3" /> */}
+                                            <Image
+                                                src={goal.imgUrl}
+                                                width={500}
+                                                height={400}
+                                                alt="Picture of the author"
+                                            />
+                                        </Tooltip.Content>
+
+                                    </Tooltip.Portal>
+                                </Tooltip.Root>
+
+                            ))}
+                        </Tooltip.Provider>
+
+                    </div>
+
+                </div>
             </div>
-            <div className='relative max-w-6xl w-full'>
-                <Progress.Root className="w-full absolute bg-gray-500 h-4 rounded-full overflow-hidden mb-56" value={props.count}>
-                    <Progress.Indicator
-                        className="bg-red-500 rounded-full w-full h-full"
-                        style={{ transform: `translateX(-${100 - currentPercent}%)` }}
-                    />
-                </Progress.Root>
-                <Tooltip.Provider>
-                    {goalValues.map(goal => (
-                        <Tooltip.Root key={goal.labelValue} open={isTooltipOpen} >
-                            <Tooltip.Trigger asChild>
-                                <div className={`absolute rounded-full bg-black h-8 w-8 -top-2 left-[${goal.position}] `} style={{ transform: `translateX(-${50}%)` }} />
-                            </Tooltip.Trigger>
-                            <Tooltip.Portal>
-                                <Tooltip.Content className="bg-white p-2 rounded-full" sideOffset={5} >
-                                    {goal.labelValue}
-                                    <Tooltip.Arrow className="fill-white" />
-                                </Tooltip.Content>
+        </div >
 
-                            </Tooltip.Portal>
-                            <Tooltip.Portal>
-                                <Tooltip.Content className="bg-white p-2 rounded-xl" sideOffset={10} side='bottom' forceMount align='end' alignOffset={-5} avoidCollisions={false}>
-                                    <div className=' lg:w-40 lg:h-40 w-20 h-20'></div>
-                                    <Tooltip.Arrow className="fill-white w-3 h-3" />
-                                </Tooltip.Content>
-
-                            </Tooltip.Portal>
-                        </Tooltip.Root>
-
-                    ))}
-                </Tooltip.Provider>
-
-            </div>
-
-        </div>
     )
 }
 
